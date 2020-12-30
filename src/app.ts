69c2eef5c33ee7e5264836ceb87b077832de85be
Kelvin Brito <britok30@gmail.com>
2020-12-30 00:39:23 -0500

    set up design + ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
 // Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+
+    const enteredAddress = addressInput.value;
+
+    // send this to Google's Api
+}
+
+form.addEventListener('submit', searchAddressHandler);
